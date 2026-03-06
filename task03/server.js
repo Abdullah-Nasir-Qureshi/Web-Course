6b156716bc896e89b62b2aa7e5731a76b2b0fb3a
Abdullah-NasirQureshi <qureshiabdullah038@gmail.com>
2026-03-06 21:16:19 +0500

    Final

--- task03/server.js
+++ task03/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB')
+mongoose.connect('mongodb://localhost:27017/')
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 

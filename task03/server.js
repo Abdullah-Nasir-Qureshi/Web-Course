8e959b9d1948baca3307bbe495290374cbab0868
Abdullah-NasirQureshi <qureshiabdullah038@gmail.com>
2026-03-06 21:07:55 +0500

    Added MongoDB connection using Mongoose

--- task03/server.js
+++ task03/server.js
@@ -4,6 +4,17 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB')
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
+// Define User Schema
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+const UserModel = mongoose.model('User', userSchema);
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
